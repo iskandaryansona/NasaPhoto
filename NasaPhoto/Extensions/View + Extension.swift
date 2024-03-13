d27f9a10f1e1d23af6da360a52682988bd031d89
iskandaryansona <iskandaryansona1999@gmail.com>
2024-03-14 01:40:45 +0400

    first commit

--- NasaPhoto/Extensions/View + Extension.swift
+++ NasaPhoto/Extensions/View + Extension.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import UIKit
+
+@IBDesignable extension UIView {
+    @IBInspectable var shadowColor: UIColor?{
+        set {
+            guard let uiColor = newValue else { return }
+            layer.shadowColor = uiColor.cgColor
+        }
+        get{
+            guard let color = layer.shadowColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+
+    @IBInspectable var shadowOpacity: Float{
+        set {
+            layer.shadowOpacity = newValue
+        }
+        get{
+            return layer.shadowOpacity
+        }
+    }
+
+    @IBInspectable var shadowOffset: CGSize{
+        set {
+            layer.shadowOffset = newValue
+        }
+        get{
+            return layer.shadowOffset
+        }
+    }
+
+    @IBInspectable var shadowRadius: CGFloat{
+        set {
+            layer.shadowRadius = newValue
+        }
+        get{
+            return layer.shadowRadius
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat{
+        set {
+            layer.cornerRadius = newValue
+        }
+        get{
+            return layer.cornerRadius
+        }
+    }
+}
